06fec8281bcd11fb0535b5d813c2409710ea35e3
Hamish Lacmane <hamish_5@hotmail.co.uk>
2023-03-30 21:40:36 +0100

    - Pet stores added

---
+++ configs/pet_stores.js
@@ -0,0 +1,25 @@
+// List of Pet stores in JSON format to help with processing in code.
+pet_stores_list = [
+    {
+        "title": "Little Paws",
+        "pictures": [
+            "https://i.imgur.com/SFT40oS.jpeg"
+        ],
+        "x": 4318,
+        "y": 4339,
+        "description": null,
+        "wheelchair_accessible_partly": true,
+        "wheelchair_accessible_fully": true
+    },
+    {
+        "title": "Paleto Pets",
+        "pictures": [
+            "https://i.imgur.com/3RVy5OX.jpeg"
+        ],
+        "x": 6653.5,
+        "y": 3494.25,
+        "description": null,
+        "wheelchair_accessible_partly": false,
+        "wheelchair_accessible_fully": false
+    }
+]
