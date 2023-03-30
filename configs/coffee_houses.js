2d0556597ab42ca9012c5c33c3c59b5a2dc0e2a8
Hamish Lacmane <hamish_5@hotmail.co.uk>
2023-03-30 22:17:12 +0100

    - Coffee houses added

---
+++ configs/coffee_houses.js
@@ -0,0 +1,57 @@
+// List of Cofee houses in JSON format to help with processing in code.
+coffee_houses_list = [
+    {
+        "title": "Bean Machine",
+        "pictures": null,
+        "x": 2762,
+        "y": 3265,
+        "description": "We're Always Open!",
+        "wheelchair_accessible_partly": false,
+        "wheelchair_accessible_fully": false
+    },
+    {
+        "title": "HeroWine",
+        "pictures": null,
+        "x": 2841,
+        "y": 3605,
+        "description": "Wine Bottles and Wine Glasses, so fancy!",
+        "wheelchair_accessible_partly": false,
+        "wheelchair_accessible_fully": false
+    },
+    {
+        "title": "Queenies",
+        "pictures": null,
+        "x": 1742.25,
+        "y": 2822.5,
+        "description": null,
+        "wheelchair_accessible_partly": false,
+        "wheelchair_accessible_fully": false
+    },
+    {
+        "title": "Pandia Expresso",
+        "pictures": null,
+        "x": 1879,
+        "y": 3280,
+        "description": "Coffee & Cigarettes",
+        "wheelchair_accessible_partly": false,
+        "wheelchair_accessible_fully": false
+    },
+    {
+        "title": "Erudite Eats",
+        "pictures": null,
+        "x": 1701,
+        "y": 2906,
+        "description": "https://i.imgur.com/t8ECMch.jpeg",
+        "wheelchair_accessible_partly": false,
+        "wheelchair_accessible_fully": false
+    },
+    {
+        "title": "The White Rabbit",
+        "pictures": null,
+        "x": 4991.5,
+        "y": 4886,
+        "description": null,
+        "wheelchair_accessible_partly": false,
+        "wheelchair_accessible_fully": false
+    }
+]
